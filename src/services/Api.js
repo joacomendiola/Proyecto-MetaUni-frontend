5a471d477bd0b0e9e2f492fe230b6b0262aab1b4
joacomendiola <joakomendiola25@gmail.com>
2025-09-27 14:39:07 -0300

    eliminar api/

--- src/services/Api.js
+++ src/services/Api.js
@@ -148,20 +148,19 @@ export async function deleteMateria(id) {
 
 // ================== USUARIOS ==================
 export async function getUsuarios() {
-  return request("/api/usuarios"); 
+  return request("/usuarios");  
 }
-
 export async function getUsuario(id) {
-  return request(`/api/usuarios/${id}`); 
+  return request(`/usuarios/${id}`);  
 }
 
 export async function updateUsuario(id, usuario) {
-  return request(`/api/usuarios/${id}`, { 
+  return request(`/usuarios/${id}`, { 
     method: "PUT",
     body: JSON.stringify(usuario),
   });
 }
 
 export async function deleteUsuario(id) {
-  return request(`/api/usuarios/${id}`, { method: "DELETE" }); 
+  return request(`/usuarios/${id}`, { method: "DELETE" });  
 }
